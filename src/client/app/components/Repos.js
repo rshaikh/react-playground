07e68d8a793924d5d47aa2edbbe7a87a83c689a8
rashid <rshaikh@equalexperts.com>
2016-09-17 21:44:20 +0530

    Add and configure react-router

---
+++ src/client/app/components/Repos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Repos extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        return (
+            <div>Repos</div>
+        );
+    }
+
+}
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Layout from './components/Layout';
+import About from './components/About';
+import Repos from './components/Repos';
+import {Router, Route, hashHistory} from 'react-router';
 
 
-ReactDom.render(<Layout />, document.getElementById('app'));
+ReactDom.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}></Route>
+        <Route path="/repos" component={Repos}></Route>
+        <Route path="/about" component={About}></Route>
+    </Router>
+), document.getElementById('app'));
